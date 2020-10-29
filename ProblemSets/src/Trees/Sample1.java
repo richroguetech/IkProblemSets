bbb4cdade777ee9bd56c5069672bc1a6a2fd68dd
Richard Brett <rbrett@classpass.com>
2020-10-29 02:46:20 -0400

    problem 3.

--- ProblemSets/src/Trees/Sample1.java
+++ ProblemSets/src/Trees/Sample1.java
@@ -1,5 +1,7 @@
 package Trees;
 
+import jdk.nashorn.internal.runtime.StoredScript;
+
 import java.util.*;
 
 public class Sample1
@@ -63,6 +65,70 @@ public class Sample1
     }
 
 
+    static boolean isBSTIterative(Solution.TreeNode root){
+        System.out.println("here at beginning");
+       Stack<Solution.TreeNode> stack = new Stack<>();
+       int left_ptr_val = Integer.MIN_VALUE;
+
+       while (!stack.isEmpty() || root != null) {
+           while (root != null) {
+               stack.push(root);
+               root = root.left_ptr;
+           }
+           root = stack.pop();
+
+           if (root.val <= left_ptr_val) {
+               return false;
+           }
+           left_ptr_val = root.val;
+           root = root.right_ptr;
+       }
+       return true;
+    }
+
+    static int[] postorderTraversal(Solution.TreeNode root){
+        // Create two stacks
+        List<Integer> returnList = new ArrayList<>();
+
+        Stack<Solution.TreeNode> s1, s2;
+
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+
+        if (root == null)
+            return null;
+
+        // push root to first stack
+        s1.push(root);
+
+        // Run while first stack is not empty
+        while (!s1.isEmpty()) {
+            // Pop an item from s1 and push it to s2
+            Solution.TreeNode temp = s1.pop();
+            s2.push(temp);
+
+            // Push left and right children of
+            // removed item to s1
+            if (temp.left_ptr != null)
+                s1.push(temp.left_ptr);
+            if (temp.right_ptr != null)
+                s1.push(temp.right_ptr);
+        }
+
+        // Print all elements of second stack
+        while (!s2.isEmpty()) {
+            Solution.TreeNode temp = s2.pop();
+            returnList.add(temp.val);
+        }
+        int[] ints = new int[returnList.size()];
+        int i = 0;
+        for (Integer n : returnList) {
+            ints[i++] = n;
+        }
+        return ints;
+    }
+
+
     public static void main(String [] args)
     {
         /*
@@ -74,7 +140,8 @@ public class Sample1
                 try{
                     //1. problem 1 - solveSingleValueTree();
                     //2. problem 2 - solveIsBST();
-                    solveIsBST();
+                    //2. problem 2- solveIsBSTIterative();
+                    solvePostorderTraversal();
                 }
                 catch(Exception e){
                     e.printStackTrace();
@@ -97,6 +164,41 @@ public class Sample1
         try{
             Solution.TreeNode root = Solution.readBinaryTree();
             boolean result = isBST(root);
+            System.out.println(result);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void solveIsBSTIterative(){
+        try{
+            Solution.TreeNode root = Solution.readBinaryTree();
+            boolean result = isBSTIterative(root);
+            System.out.println(result);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void printArray(int[] result){
+        if(result == null){
+            System.out.println();
+            return;
+        }
+        for(int i = 0; i < result.length; i++){
+            if(i>0){
+                System.out.print(" ");
+            }
+            System.out.print(result[i]);
+        }
+        System.out.println();
+    }
+
+    public static void solvePostorderTraversal(){
+        try{
+            Solution.TreeNode root = Solution.readBinaryTree();
+            int[] result = postorderTraversal(root);
+            printArray(result);
             System.out.println(result);
         }catch (Exception e) {
             System.out.println(e.getMessage());
